99a7069a1fde8608d36d86a392be3d546bc78ed9
Tejas Coder <pattnaiktajeas527@gmail.com>
2026-06-25 12:05:51 +0530

    hhhhhh

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,24 +1,13 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://127.0.0.1:8000'
+const BASE_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 })
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token")
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-
-    return config
-  },
-  (error) => Promise.reject(error)
-)
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
