93ba5a254bb39d5c183c9ad4106893f3f721e696
Tejas Coder <pattnaiktajeas527@gmail.com>
2026-06-25 12:10:25 +0530

    hgf

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://finxtime.onrender.com";
 
 export const registerUser = async (userData) => {
   const response = await axios.post(
